d2a10acaf275def80bcf758979e3aea318183fd7
12513 <matukevich@mingas.by>
2026-05-19 16:03:46 +0300

    fix: add Swagger for gloval-search

--- src/modules/global-search/global-search.controller.ts
+++ src/modules/global-search/global-search.controller.ts
@@ -2,12 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { GlobalSearchService } from './global-search.service';
 import { SearchFileDto } from '../document-version';
 import { SearchResultDTO } from './dto/search-result.dto';
+import { ApiBearerAuth, ApiBody, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('global-search')
 export class GlobalSearchController {
     constructor(private readonly globalSearchService: GlobalSearchService) {}
+
     @Post()
+    @ApiBearerAuth('access-token')
+    @ApiOperation({ summary: 'Поиск по всем документам' })
+    @ApiBody({ type: SearchFileDto })
     async globalSearch(@Body() query: SearchFileDto): Promise<SearchResultDTO[]> {
-        return await this.globalSearchService.globalSearch(query);
+        return this.globalSearchService.globalSearch(query);
     }
 }
